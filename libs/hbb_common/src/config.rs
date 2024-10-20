67755ddad9e34fc74ee666d536d4ac5257ce2d55
nccstar <100890905+nccstar@users.noreply.github.com>
2024-10-20 16:04:17 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nccstar.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "nccstarGDNiFxR9aBAoE0p0K5usboLbz4tqmzvJHCN7K1oGYfQ";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
