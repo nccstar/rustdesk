ea6611117127b4c14862c926b64aa622c7e188f9
nccstar <100890905+nccstar@users.noreply.github.com>
2024-10-20 19:36:17 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://nccstar.top".to_owned()
+    "http://nccstar.top:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
