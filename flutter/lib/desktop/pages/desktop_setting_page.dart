a9c0b17b17ea47d340bfb3ab671f9298d4dbef8f
nccstar <100890905+nccstar@users.noreply.github.com>
2024-12-14 10:52:46 +0800

    Update desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1847,7 +1847,7 @@ class _AboutState extends State<_About> {
       final scrollController = ScrollController();
       return SingleChildScrollView(
         controller: scrollController,
-        child: _Card(title: translate('About RustDesk'), children: [
+        child: _Card(title: translate('亿芯电子远程被控端18603549494'), children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
